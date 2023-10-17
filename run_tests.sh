4b602fe1662be9f9171410ccd6e4cb2f2202adf5
Grimmer <grimmer0125@gmail.com>
2023-10-17 19:02:15 +0800

    chore: store all log to file only when jest fails

--- run_tests.sh
+++ run_tests.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-# Run Jest tests and store the exit code (0 for success, 1 for failure)
 # jest output is usually in stderr, even if the test passes
-npm test -- test/example.test.js > stdout.txt 2> stderr.txt
+# npm test -- test/example.test.js > stdout.txt 2> stderr.txt
 # https://stackoverflow.com/questions/6674327/redirect-all-output-to-file-in-bash
-# npm test -- test/example.test.js  > allout.txt 2>&1
-all_log=$(npm test -- test/example.test.js 2>&1) 
-TEST_RESULT=$?
 
-# Check the exit code and print the appropriate message
-if [ $TEST_RESULT -eq 0 ]; then
-    echo "Tests succeeded"
+currentDate=$(date +%Y-%m-%d-%H-%M-%S)
+log_file=${currentDate}s.txt #"$currentDate-s.txt"
+echo $currentDate
+echo $log_file
+test_file_name="test/example.test.js"
+echo $test_file_name
+
+# npm test -- test/example.test.js  > $currentDate 2>&1
+# converting "all_log" to log file later might be  OK 
+# but all_log already misse some format, e.g. breakline
+# so store the log_file and all_log var initially
+all_log=$(npm test -- $test_file_name 2>&1 | tee $log_file) 
+EXIT_CODE=$?
+
+STR='GNU/Linux is an operating system'
+SUB="FAIL $log_file_name"
+echo $SUB
+# we can check "PASS test/example.test.js" or "FAIL test/example.test.js"
+if grep -q "$SUB" <<< "$all_log"; 
+then
+    echo "It's fail case"
 else
-    echo "Tests failed"
+    echo "It's sucess case"
+    # delete the file 
+    rm $log_file
 fi
 
 echo 'print save log'
-# we can check "PASS test/example.test.js" or "FAIL test/example.test.js"
-echo ${all_log}
+# echo ${all_log}
 
-# Exit with the same code as Jest (optional but can be useful)
-exit $TEST_RESULT
+exit $EXIT_CODE
